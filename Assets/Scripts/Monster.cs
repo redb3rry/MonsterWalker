3a6049fd0f3e6b37fb5d2d091fa4fe5a094d0eef
redb3rry <michalmalinowski99@yahoo.co.uk>
2020-03-16 22:57:37 +0100

    Quick test of building on android

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    //config
+    [SerializeField] float monsterSpeed = 1f;
+    //references
+    Player player;
+    //states
+    Vector2 playerToMonsterVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody2D rigibody = GetComponent<Rigidbody2D>();
+        Vector2 playerPos = new Vector2(player.transform.position.x, player.transform.position.y);
+        playerToMonsterVector = this.transform.position - player.transform.position;
+        rigibody.AddForce(monsterSpeed * playerToMonsterVector);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //config
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 9f;
+    [SerializeField] float screenWidthInUnits = 9f;
+
+    //state
+    private Touch touch;
+    private Vector2 playerPos; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        touch = Input.GetTouch(0);
+        playerPos = new Vector2(transform.position.x, transform.position.y);
+        playerPos.x = Mathf.Clamp(GetPositionX(), minX, maxX);
+        transform.position = playerPos;
+    }
+    private float GetPositionX()
+    {
+        return touch.position.x / Screen.width * screenWidthInUnits;
+    }
+}
