b08d09873b743daa34c84340bc0f31b67128b371
redb3rry <michalmalinowski99@yahoo.co.uk>
2020-03-21 01:14:15 +0100

    Added life meter, game over screen. Game is now playable.

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private Sprite[] lifeSprites;
+    [SerializeField] private int lives = 3; //TODO unserialize
+    [SerializeField] private Image image;
+    private float timer;
+    [SerializeField] private SceneLoader sceneLoader;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        timerText.text = timer.ToString("n2");
+    }
+    public void UpdateLives(int livesLost)
+    {
+        if (lives == 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
+
+        lives -= livesLost;
+        image.sprite = lifeSprites[lives];
+    }
+}
--- Assets/Scripts/PoopScroll.cs
+++ Assets/Scripts/PoopScroll.cs
@@ -7,15 +7,27 @@ public class PoopScroll : MonoBehaviour
     [SerializeField] public float poopSpeed;
     [SerializeField] private Renderer poopRenderer;
     [SerializeField] private Collider2D poopCollider;
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameLogic gameLogic;
+    [SerializeField] private int damage = 1;
 
-    // Update is called once per frame
+    private void Start()
+    {
+        gameLogic = FindObjectOfType<GameLogic>();
+    }
     void Update()
     {
         poopRenderer.material.mainTextureOffset += new Vector2(0f, poopSpeed * Time.deltaTime);
         poopCollider.offset -= new Vector2(0f, poopSpeed * Time.deltaTime);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player" || collision.gameObject.name == "Monster")
+        {
+            gameLogic.UpdateLives(damage);
+        }
+        if (collision.gameObject.name == "Player" || collision.gameObject.name == "Monster")
+        {
+            Destroy(gameObject);
+        }
     }
 }
